c75297da78e640a2dc2b4205ba27f7df31f2dece
XUANXUQAQ <xuanxusb@outlook.com>
2023-06-30 19:34:09 +0800

    删除错误的测试

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -1,10 +1,6 @@
-import FileEngine.Web.Plugin.PluginMain;
-
 public class TestMain {
 
     public static void main(String[] args) {
-        PluginMain pluginMain = new PluginMain();
-        pluginMain.loadPlugin();
-        pluginMain.textChanged("test");
+
     }
 }
